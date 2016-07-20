75c188cdd122d8750044e463cf361ba178872abf
Anna Lisowska <anna-lisowska@outlook.com>
2016-07-20 18:47:08 +0100

    get_context_data in BookView(CreateView) works

--- club/views.py
+++ club/views.py
@@ -1,15 +1,15 @@
 """This is a module to determine which data will be displayed"""
 import datetime
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.generic import TemplateView, View
+#from django.http import HttpResponseRedirect
+#from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
 #from django.views.generic.edit import FormMixin
 #from django.views.generic.detail import SingleObjectMixin
 #from django.template.response import TemplateResponse
-from django.shortcuts import get_object_or_404
-from django.contrib import messages
+#from django.shortcuts import get_object_or_404
+#from django.contrib import messages
 #from django.core.urlresolvers import reverse
-from .models import Training
+from .models import Training, Booking
 from .forms import BookingForm
 
 
@@ -23,36 +23,49 @@ class HomePageView(TemplateView):
         context['trainings'] = Training.objects.filter(state="A", training_date__gte=now).order_by('training_date', 'start_time')
         return context
 
-class BookView(View):
+
+class BookView(CreateView):
     """
     Booking page - getting data of trainings and
-    posting data of participant to db
+    posting data of booking to db
     """
+
     template_name = 'club/book.html'
     form_class = BookingForm
-        
+    success_url = '/success/'
+
+    """ 
     def post(self, request, training_id):
         form = self.form_class(request.POST)
+        training = get_object_or_404(Training,pk = training_id)
         if form.is_valid():
             form.save()
             messages.success(request, 'Well done! Fitness Class has been booked for you. If you would like to cancel booked fitness class, please contact our office.')
             return HttpResponseRedirect('/book/' + training_id)
-            #return render(request, self.template_name, {'form': form})
         else:
-            #form = self.form_class()
             messages.error(request, 'Oh snap! Fill/correct all fields and try submitting again.')
-            #return HttpResponseRedirect('/book/' + training_id)
-            #return render(request, self.template_name, {'form': form})
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {'form': form, 'training': training})
        
     def get(self, request, training_id):
-        form = self.form_class()
         training = get_object_or_404(Training,pk = training_id)
         form = self.form_class(instance=training)
         context = {'training': training,
                    'form': form}
         return render(request, self.template_name, context)
-       
+    """
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super(BookView, self).get_context_data(**kwargs)
+        context['training'] = Training.objects.get(pk=self.kwargs['training_id'])
+        return context
+
+class SuccessView(TemplateView):
+    template_name = 'club/success.html'
+    
+
+    
+        
 
 
 
